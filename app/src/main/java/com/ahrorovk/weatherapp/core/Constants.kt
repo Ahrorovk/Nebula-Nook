e5460ba8db5021f8a027f8d3fe5b9e0b7c17e28b
Ahrorovk <akhrorovk5758@gmail.com>
2023-11-29 01:50:21 +0500

    add apk

--- app/src/main/java/com/ahrorovk/weatherapp/core/Constants.kt
+++ app/src/main/java/com/ahrorovk/weatherapp/core/Constants.kt
@@ -2,8 +2,6 @@ package com.ahrorovk.weatherapp.core
 
 object Constants {
     const val WEATHER_BASE_URL = "https://api.open-meteo.com/"
-    const val LOCATION_BY_CITY_BASE_URL = "http://api.openweathermap.org/"
-    const val api_key = "1b2620bae8cbf416d02d43f3e6432845"
     const val hourlyFields =
         "temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl"
 }
